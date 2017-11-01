7427f576b036fc61a924a9b3740dcda2e7a01f2e
eriksmith1998 <33281378+eriksmith1998@users.noreply.github.com>
2017-11-01 17:04:04 +0100

    pagina's aangemaakt, input types

---
+++ page/login.php
@@ -0,0 +1,12 @@
+<h1> inlogformulier </h1>
+
+
+	<form method="post" action="login_controle.php">
+	<table>
+	<tr><td>Gebruikersnaam:</td><td> <input name="gebruikersnaam" type="text"  size="20" ></td></tr>
+	<tr><td>Wachtwoord: </td><td><input name="wachtwoord" type="password" size="20" maxlength="8"></td></tr>
+	</table>
+	<br>
+	<input type="submit" name="Submit" value="Inloggen">
+	<input name="reset" type="reset" id="reset" value="Leegmaken">
+	</form>
