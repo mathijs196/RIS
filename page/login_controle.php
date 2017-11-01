ef6fcb047f67337b7bd8c0ffb00e1efa2f2d63ed
eriksmith1998 <33281378+eriksmith1998@users.noreply.github.com>
2017-11-01 17:18:55 +0100

    login_controle

--- page/login_controle.php
+++ page/login_controle.php
@@ -11,3 +11,43 @@ FUNCTIES:
 
 -->
 
+<?php
+session_start(); // sessie beginnen
+include ('');
+
+// controleren of pagina correct is aangeroepen.
+if (!empty($_POST)){
+	
+	$gebruiker = mysqli_real_escape_string($db, $_POST['gebruikersnaam']);
+	$wachtwoord = mysqli_real_escape_string($db, $_POST['wachtwoord']);
+	$query = 	"SELECT * FROM users 
+				WHERE gebruikersnaam ='" . $_POST["gebruikersnaam"] ."'
+				AND wachtwoord='" . $_POST["wachtwoord"] ."'"; 
+	$result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error()); 
+		
+	if (mysqli_num_rows($result) > 0){
+        // gebruikersnaam gevonden, registreer gegevens in session
+				$_SESSION["auth"]=true; //auth controleert of een klant is ingelogd
+				$_SESSION["timeout"]=time() + 120; 
+				$_SESSION["gebruiker"]=$gebruiker;
+		while($row = mysqli_fetch_assoc($result)) {
+		$rol = $row['rol'];
+	}
+		// Doorsturen naar beveiligde pagina
+	if(($rol) == "admin") {
+        header("Location: admin.php"); 
+        exit(); 
+	}
+	elseif(($rol =="medewerker")) {
+		header("Location: medewerker.php"); 
+        exit(); 
+	}
+	
+}else{
+  	$tekst = "Gebruikersnaam en/of wachtwoord incorrect";
+			die($tekst);
+		}
+}else{
+	
+}
+?>
