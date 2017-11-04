3c338ca5154e737e6fc215428499dfcd8de2f71e
eriksmith1998 <33281378+eriksmith1998@users.noreply.github.com>
2017-11-04 16:20:41 +0100

    Klikbaar gemaakt

--- page/home.php
+++ page/home.php
@@ -9,16 +9,16 @@ while($row = mysqli_fetch_assoc($result)){
 
             <div class="col-lg-4 col-md-6 mb-4">
               <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="img/<?php echo $row['afbeelding'] ?>.png" style="height: 250px; width: 250px;" alt=""></a>
+                <a href="?page=product_info&product=<?php echo $row['idproducten'];?>"><img class="card-img-top" src="img/<?php echo $row['afbeelding'] ?>.png" style="height: 250px; width: 250px;" alt=""></a>
                 <div class="card-body">
                   <h4 class="card-title">
-                    <a href="#"><?php echo $row['naam'] ?></a>
+                    <a href="?page=product_info&product=<?php echo $row['idproducten'];?>"><?php echo $row['naam'] ?></a>
                   </h4>
                   <h5>&euro;<?php echo $row['prijs'] ?></h5>
                   <p class="card-text"><?php echo $row['omschrijving'] ?></p>
                 </div>
                 <div class="card-footer">
-                  <button class="btn btn-success">In winkelwagen</button>
+                  <button class="btn btn-success">In winkelwagen</button> <a href="?page=product_info&product=<?php echo $row['idproducten'];?>" class="btn btn-success">Bekijk product</a>
                 </div>
               </div>
             </div>
