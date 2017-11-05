36ef2334c35c56dc9a1be99a67a14bb30db9ba7e
eriksmith1998 <33281378+eriksmith1998@users.noreply.github.com>
2017-11-05 16:13:45 +0100

    .

--- index.php
+++ index.php
@@ -63,37 +63,37 @@ FUNCTIES:
 
     <!-- Page Content -->
     <div class="container">
-<div class="row">	
-		<div class="col-lg-3">
-		<?php include 'page/form_login.php' ?>
-          <br><br>
-          <div class="list-group">
-		 
-			<?php
-			$query = 	"SELECT * FROM catogorie"; 
-					$result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error()); 
-					
-					while($row = mysqli_fetch_assoc($result)){
+		<div class="row">	
+			<div class="col-lg-3">
+			<?php include 'page/form_login.php' ?>
+			  <br><br>
+			  <div class="list-group">
+			 
+				<?php
+				$query = 	"SELECT * FROM catogorie"; 
+						$result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error()); 
+						
+						while($row = mysqli_fetch_assoc($result)){
+				?>
+				<a href="?page=home&categorie=<?php echo $row['idcategorie']; ?>" class="list-group-item"><?php echo $row['naam']; ?></a>
+						
+				<?php
+			
+				}
+				?>
+			  </div>
+			</div>
+			
+			<?php 
+				if(!empty($_GET)){
+				$page = $_GET['page'];
+				include "page/$page.php";
+				}else{
+				include "page/home.php";
+				}
 			?>
-            <a href="?page=home&categorie=<?php echo $row['idcategorie']; ?>" class="list-group-item"><?php echo $row['naam']; ?></a>
-            		
-			<?php
-		
-			}
-			?>
-          </div>
-        </div>
-		
-		<?php 
-			if(!empty($_GET)){
-			$page = $_GET['page'];
-			include "page/$page.php";
-			}else{
-			include "page/home.php";
-			}
-		?>
-		
-	</div>	
+			
+		</div>	
     </div>
     <!-- /.container -->
 
@@ -103,7 +103,7 @@ FUNCTIES:
 
         <p class="m-0 text-center text-white">Copyright &copy; Rick Sanchez</p>
 
-        <p class="m-0 text-center text-white">Copyright &copy; Ricks Interdimensional Shop 2017</p>
+      
 
       </div>
       <!-- /.container -->
