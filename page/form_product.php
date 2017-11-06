90c28f142e92730b17108742f1454eb1d2370194
LarsRoffel <larsroffel2@gmail.com>
2017-11-06 19:03:20 +0100

    form
    product form input aangepast

--- page/form_product.php
+++ page/form_product.php
@@ -9,12 +9,12 @@ FUNCTIES:
 
 -->
 
-<?php 
+<?php
 
 	if(isset($_SESSION[''])){
 	echo "Er is al een product genaamd: ". $_SESSION["productnaam"] ;
-	}else{ 
-	
+	}else{
+
 	?>
 <div class="col-lg-9">
 	<div class="card card-outline-secondary my-4">
@@ -22,22 +22,22 @@ FUNCTIES:
 				  Contact Pagina
 				</div>
 				<div class="card-body">
-				 
+
 
 	<form method="post" action="?page=product_toevoegen">
 	<fieldset>
 		<legend>Persoonlijke informatie: </legend>
 		<table>
-			<tr><td>Productcode</td><td>			<input name="idproducten" type="number" size="40" maxlength="20" required></td></tr>
-			<tr><td>Productnaam:*</td><td>			<input name="naam" type="text" size="40" maxlength="20" required></td></tr>
+			<tr><td>Productcode</td><td>			<input name="idproducten" type="number" size="40" maxlength="20" ></td></tr>
+			<tr><td>Productnaam:</td><td>			<input name="naam" type="text" size="40" maxlength="20" ></td></tr>
 			<tr><td>Afbeelding:</td><td> <input name="afbeelding" type="text" size="40" maxlength="12" ></td></tr>
-			<tr><td>Omschrijving:*</td><td>	<input name="omschrijving" type="text" size="40" maxlength="20" required></td></tr>
-			<tr><td>Specificatie</td><td>		<input name="specificaties" type="text" size="40" placeholder="9999AA" maxlength="6" required></td></tr>
-			<tr><td>Prijs</td><td>	<input name="prijs" type="text" size="40" maxlength="25" required></td></tr>
-			<tr><td>Hoeveelheid</td><td>		<input name="hoeveelheid" type="text" size="40" maxlength="25" required></td></tr>
+			<tr><td>Omschrijving:</td><td>	<input name="omschrijving" type="text" size="40" maxlength="20" </td></tr>
+			<tr><td>Specificatie</td><td>		<input name="specificaties" type="text" size="40" maxlength="6"></td></tr>
+			<tr><td>Prijs</td><td>	<input name="prijs" type="text" size="40" maxlength="25" ></td></tr>
+			<tr><td>Hoeveelheid</td><td>		<input name="hoeveelheid" type="text" size="40" maxlength="25"></td></tr>
 		</table>
-	</fieldset>	
-		
+	</fieldset>
+
 	<br>
 
 
@@ -47,8 +47,8 @@ FUNCTIES:
 
 				</div>
 	</div>
-</div>	
-<?php 
+</div>
+<?php
 }
 
 ?>
